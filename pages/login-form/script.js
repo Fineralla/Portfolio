cb0e464313c25fab12660c016be33cd8ba5d80f9
Fineralla <fineralla@inbox.ru>
2024-04-22 19:22:35 +0300

    add footer

--- pages/login-form/script.js
+++ pages/login-form/script.js
@@ -1,32 +1,31 @@
-const wrapper = document.querySelector('.wrapper');
-const registerLink = document.querySelector('.register-link');
-const loginLink = document.querySelector('.login-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const btnClose = document.querySelector('.icon-close');
+const wrapper = document.querySelector(".wrapper");
+const registerLink = document.querySelector(".register-link");
+const loginLink = document.querySelector(".login-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const btnClose = document.querySelector(".icon-close");
 
 console.log(loginLink);
 console.log(registerLink);
 
-registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active');
-  console.log('clicked register');
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
 });
 
-loginLink.addEventListener('click', () => {
-  wrapper.classList.remove('active');
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
 });
 
-btnPopup.addEventListener('click', () => {
-  wrapper.classList.add('active-popup');
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
 });
 
-btnClose.addEventListener('click', () => {
-  wrapper.classList.remove('active-popup');
+btnClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
 });
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener("keydown", function (e) {
   console.log(e);
   if (e.which === 27) {
-    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove("active-popup");
   }
 });
